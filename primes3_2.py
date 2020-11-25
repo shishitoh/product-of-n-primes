8e253fcbdd642b78b07ebf3915c1381af4c29de1
shishitoh <txoeznaon@gmail.com>
2020-11-25 14:57:58 +0900

    コミット

--- primes3_2.py
+++ primes3_2.py
@@ -17,25 +17,29 @@ def primes3_2(pf, n):
 	appendPF = PF.append
 
 	func1 = lambda x: (P[x]**2 < -(-n >> (pf-2)))
-	func2 = lambda x: (P[x+j]*p < -(-n >> (pf-2)))
+	func2 = lambda x: (P[x]*p < -(-n >> (pf-2)))
+	high = None
 	for j, p in enumerate(bitakewhile(P, func1)):
-		appendPF(bitakewhile(P[j:], func2) * p)
+		high = biS(P, func2, low=j-1, high=high)
+		appendPF(P[j:high] * p)
 
 	func1 = lambda x: (PF[x][0]*P[x] < sup)
 	func2 = lambda x: (PF[j][x]*p < sup)
 	func3 = lambda x: (PF[x][0]*p < sup)
 	func4 = lambda x: (S[x]*p < sup)
+	high1 = None
 	for i in range(3, pf+1):
 		sup = -(-n >> (pf-i))
-		tophigh = biS(P, func1, high=len(PF))
-		for j, p in enumerate(P[:tophigh]):
+		high1 = biS(PF, func1, high=high1)
+		high3 = None
+		for j, p in enumerate(P[:high1]):
 			PF[j] = [bitakewhile(PF[j], func2) * p]
-			high = biS(PF, func3, low=j)
-			for S in PF[j+1:high]:
+			high3 = biS(PF, func3, low=j, high=high3)
+			for S in PF[j+1:high3]:
 				PF[j].append(bitakewhile(S, func4) * p)
 			PF[j] = np.concatenate(PF[j])
 			PF[j].sort()
-		PF = PF[:tophigh]
+		PF = PF[:high1].copy()
 	del P
 
 	PF = np.concatenate(PF)
